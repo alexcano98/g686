696f65688e67811e4d9bcf4563b772df856ab699
Carly <okridgway@gmail.com>
2019-03-04 11:51:18 +0100

    practica4 branch

---
+++ practicas/practica4/ex4.py
@@ -0,0 +1,123 @@
+#!/opt/local/bin/python2.7
+# -*- coding : utf-8 -*-
+
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the
+#  exercise. You will need to complete the following functions:
+#
+#     linear_kernel
+#     polynomial_kernel
+#     rbf_kernel
+#     sigmoid_kernel
+#     datasetParams
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+
+import numpy as np
+import matplotlib.pylab as pl
+from sklearn import svm
+from practica4 import linear_kernel, polynomial_kernel, rbf_kernel, sigmoid_kernel, datasetParams
+
+def loadData(filename):
+    print('Loading data ...')
+    data=np.loadtxt(filename,delimiter=",")
+    m,p=data.shape
+    X=data[:,:-1]
+    y=data[:,-1:].reshape(m,)
+    return m,p,X,y
+
+
+##################### This part is for visualization only ###########################
+
+m,n,X,y = loadData('ex4data1.txt')
+
+
+
+kernelType="linear"
+##kernelType="poly"
+##kernelType="sigmoid"
+##kernelType="rbf"
+
+clf = svm.SVC(kernel=kernelType,C=1)
+
+h = .02  # step size in the mesh
+
+clf.fit(X, y)
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+pl.pcolormesh(xx, yy, Z, cmap=pl.cm.Paired)
+
+# Plot also the training points
+pl.scatter(X[:, 0], X[:, 1], c=y, cmap=pl.cm.Paired)
+# Change the above line with
+##pl.scatter(X[:, 0], X[:, 1], c=y)
+#if you want to see the points in different colors
+pl.title('SVM classification using '+kernelType+' kernel')
+pl.axis('tight')
+pl.show()
+
+########################  Here is where your exercice starts #######################
+
+m,n,X,y = loadData('ex4data2.txt')
+
+print('\nChecking the linear kernel...\n')
+clf = svm.SVC(kernel="linear")
+clf.fit(X, y)
+print('The accuracy of the SVM trained with predefined linear kernel is: ', clf.score(X,y))
+clf = svm.SVC(kernel=linear_kernel)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with YOUR linear kernel is: ', clf.score(X,y))
+
+
+print('\nChecking the polynomial kernel...\n')
+clf = svm.SVC(kernel="poly",gamma=1,degree=2,coef0=3)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with predefined polynomial kernel is: ', clf.score(X,y))
+clf = svm.SVC(kernel=polynomial_kernel)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with YOUR polynomial kernel is: ', clf.score(X,y))
+
+
+print('\nChecking the sigmoid kernel...\n')
+clf = svm.SVC(kernel="sigmoid",gamma=1,coef0=2)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with predefined sigmoid kernel is: ', clf.score(X,y))
+clf = svm.SVC(kernel=sigmoid_kernel)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with YOUR sigmoid kernel is: ', clf.score(X,y))
+
+
+print('\nChecking the rbf kernel...\n')
+clf = svm.SVC(kernel="rbf",gamma=3)
+clf.fit(X, y)
+print('The accuracy of the SVM trained with predefined rbf kernel is: ', clf.score(X,y))
+clf=svm.SVC(kernel="precomputed")
+gram=rbf_kernel(X,X)
+clf.fit(gram,y)
+print("hey")
+print('The accuracy of the SVM trained with YOUR rbf kernel is: ', clf.score(gram,y))
+
+
+
+########### This is a different dataset that you can use to experiment with ##################
+#                 Try different values of C and sigma here.
+
+m,n,X,y = loadData('ex4data1.txt')
+
+C,sigma=datasetParams(X, y)
+print('\nThe optimal values for C and sigma are,',C,' and ',sigma,', respectively')
+
---
+++ practicas/practica4/practica4.py
@@ -0,0 +1,80 @@
+#!/opt/local/bin/python2.7
+# -*- coding : utf-8 -*-
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+def linear_kernel(x,l):
+    # Initialize some useful values
+    rx,cx=x.shape
+    rl,cl=l.shape
+    # You need to return the following variable correctly 
+    result = np.zeros((rx,rl),dtype=float)
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: return K(x,l) using a linear kernel
+#               linear: < x, l>
+
+# =========================================================================      
+    return result
+
+def polynomial_kernel(x,l, gamma=1,d=2,r=3):
+    # Initialize some useful values
+    rx,cx=x.shape
+    rl,cl=l.shape
+    # You need to return the following variable correctly 
+    result = np.zeros((rx,rl),dtype=float)
+    
+# ====================== YOUR CODE HERE ======================
+# Instructions: return K(x,l) using a polynomial kernel
+#               polynomial: (gamma * < x, l > + r)^d
+
+# =========================================================================      
+    return result
+
+def sigmoid_kernel(x,l, gamma=1,r=2):
+    # Initialize some useful values
+    rx,cx=x.shape
+    rl,cl=l.shape
+    # You need to return the following variable correctly 
+    result = np.zeros((rx,rl),dtype=float)
+    
+# ====================== YOUR CODE HERE ======================
+# Instructions: return K(x,l) using a sigmoid kernel
+#               sigmoid: tanh(gamma * <x,l> + r)
+# Hint: you may find useful numpy.tanh
+
+# =========================================================================      
+    return result
+
+def rbf_kernel(x,l, gamma = 3):
+    # Initialize some useful values
+    rx,cx=x.shape
+    rl,cl=l.shape
+    # You need to return the following variable correctly 
+    result = np.zeros((rx,rl),dtype=float)
+    
+# ====================== YOUR CODE HERE ======================
+# Instructions: return K(x,l) using an rbf kernel
+#               rbf: exp(-gamma * |x-l|^2)
+# Hint: use one loop
+
+# =========================================================================      
+    return result
+
+def datasetParams(X, y):   
+    # You need to return the following variables correctly
+    # try all values in [0.01, 0.03, 0.1, 0.3, 10, 30]
+    optC = 0
+    optSigma = 0
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Fill in this function to return the optimal C and sigma
+#               learning parameters found using the cross validation set
+#               (see sklearn.train_test_split) with predefined values of
+#               parameters.
+#   Hint: you can use svm.score to compute the prediction error
+
+# =========================================================================      
+    return optC,optSigma
